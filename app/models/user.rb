6123236dc55d4b47ee5ab968191d5175852a9c9b
ariannakosten <arianna.kosten@gmail.com>
2019-09-22 19:50:15 -0700

    Done.

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,3 +2,7 @@ class User < ActiveRecord::Base
   has_many :comments
   has_many :posts, through: :comments
 end
+
+#can't just declare that our User has_many :posts 
+#because our posts table doesn't have a foreign key
+# called user_id
